cee2f7d420fa3fddbe8b08d0496c4d308e09bdff
Rituraj Navindgikar <riturajn1200@gmail.com>
2025-04-16 20:04:44 -0400

    remove ros2 build files

--- clothoid_path_ws/build/path_controller/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/rituraj/Northeastern_University/MR/final_project/clothoid_path_ws/install/path_controller'
